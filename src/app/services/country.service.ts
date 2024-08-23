a265411f075a75705864ad5695d8887d6d928e25
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-08-23 14:38:06 +0300

    v0.0.1 Service for load JSON data

---
+++ src/app/services/country.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  private jsonUrl = 'assets/data/data.json';
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(): Observable<any> {
+    return this.http.get(this.jsonUrl);
+  }
+}
